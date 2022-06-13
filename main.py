c1528f841245e7f639e22516f22039bdbc341eda
Yaika <yaikazeeman@hotmail.com>
2022-06-13 17:21:34 +0200

    the code in server.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 
 app = Flask(__name__)
 project_dir = os.path.dirname(os.path.abspath(__file__))
+
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "todo.db"))
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 db = SQLAlchemy(app)
