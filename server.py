843ef9eb7113e3428666399ddd415e24ef487776
Mario014 <mariocas1495@gmail.com>
2022-06-14 13:53:41 +0200

    firebase config, add and edit

--- server.py
+++ server.py
@@ -7,57 +7,37 @@ app = Flask(__name__)
 # Initialize Firestore DB
 
 
-cred = credentials.Certificate("./key.json")
+cred = credentials.Certificate("./newkey.json")
 firebase= initialize_app(cred)
-
 db = firestore.client()
 todo_ref = db.collection('todos')
 
 
 @app.route('/', methods=['GET'])
 def home():
+    docs = db.collection('notes').get()
+    print(docs)    
+    notes = [doc.to_dict() for doc in docs]
     
-    return render_template("index.html")
+    print(notes)
+    return render_template("index.html", notes = notes)
 
 @app.route('/add', methods=['POST'])
 def create():
-    note=jsonify(request.form['text'])
-    db.collection(u'notes').document(u'2').set(note)
-    return redirect(url_for('/'))
+    note = {
+        'text': request.form['text']
+    }
+    db.collection('notes').add(note)
+    return redirect(url_for('home'))
 
-@app.route('/list', methods=['GET'])
-def read():
-    """
-        read() : Fetches documents from Firestore collection as JSON
-        todo : Return document that matches query ID
-        all_todos : Return all documents
-    """
-    try:
-        # Check if ID was passed to URL query
-        todo_id = request.args.get('id')    
-        if todo_id:
-            todo = todo_ref.document(todo_id).get()
-            return jsonify(todo.to_dict()), 200
-        else:
-            all_todos = [doc.to_dict() for doc in todo_ref.stream()]
-            return jsonify(all_todos), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
+@app.route('/update', methods=['PUT'])
+def update(text):
+    data={
+        "text": text
+    }
+    db.collection('notes').update(data)
 
-@app.route('/update', methods=['POST', 'PUT'])
-def update():
-    """
-        update() : Update document in Firestore collection with request body
-        Ensure you pass a custom ID as part of json body in post request
-        e.g. json={'id': '1', 'title': 'Write a blog post today'}
-    """
-    try:
-        id = request.json['id']
-        todo_ref.document(id).update(request.json)
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
-@app.route('/delete', methods=['GET', 'DELETE'])
+@app.route('/delete', methods=['DELETE'])
 def delete():
     """
         delete() : Delete a document from Firestore collection
