b69df26241a2879115dc379df4d48ba4059a8435
Mario014 <mariocas1495@gmail.com>
2022-06-14 15:18:14 +0200

    change port

--- server.py
+++ server.py
@@ -50,5 +50,7 @@ def delete():
     except Exception as e:
         return f"An Error Occured: {e}"
 
+
 if __name__ == '__main__':
-    app.run(threaded=True, debug=True, host='0.0.0.0')
+    port = int(os.getenv("PORT", 8080))
+    app.run(threaded=True, debug=True, host='0.0.0.0', port=port)
