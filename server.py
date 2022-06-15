0e1121350c8932f567c981843bfb3e202b896c6e
NickDeitmers <71077922+NickDeitmers@users.noreply.github.com>
2022-06-15 15:29:06 +0200

    add UPDATE and DELETE functionality

--- server.py
+++ server.py
@@ -1,52 +1,61 @@
-from flask import Flask, redirect, render_template, request, jsonify, url_for
-from firebase_admin import credentials, firestore, initialize_app
+from flask import Flask, redirect, render_template, request, url_for
+from firebase_admin import credentials, firestore, initialize_app, _apps
 
 # Initialize Flask App
 app = Flask(__name__)
 
 # Initialize Firestore DB
-cred = credentials.Certificate("./newkey.json")
-firebase= initialize_app(cred)
-db = firestore.client()
-todo_ref = db.collection('todos')
+if not _apps:
+    cred = credentials.Certificate("./newkey.json") 
+    firebase = initialize_app(cred)
 
+db = firestore.client()
+notes_ref = db.collection('notes')
 
 @app.route('/', methods=['GET'])
 def home():
-    docs = db.collection('notes').get()
-    print(docs)    
+    docs = notes_ref.get()
     notes = [doc.to_dict() for doc in docs]
     
-    print(notes)
     return render_template("index.html", notes = notes)
 
 @app.route('/add', methods=['POST'])
-def create():
+def add():
+    """
+        add() : Add a document from Firestore collection 
+    """
     note = {
         'text': request.form['text']
     }
-    db.collection('notes').add(note)
+    app.logger.info(request.form['text'])
+    notes_ref.add(note)
     return redirect(url_for('home'))
 
-@app.route('/update', methods=['PUT'])
-def update(text):
-    data={
-        "text": text
-    }
-    db.collection('notes').update(data)
+@app.route('/update', methods=['POST'])
+def update():
+    """
+        update() : Update a document from Firestore collection 
+    """
+    app.logger.info(request.form)
 
-@app.route('/delete', methods=['DELETE'])
-def delete():
+    value = request.form["text"] 
+    data={
+        "text": f"{value} but updated"
+    }    
+    doc = notes_ref.where("text", "==", value).get()[0]
+    notes_ref.document(doc.id).update(data)
+    return redirect(url_for('home'))
+    
+
+@app.route('/delete', methods=['POST'])
+def delete(): 
     """
-        delete() : Delete a document from Firestore collection
+        delete() : Delete a document from Firestore collection 
     """
-    try:
-        # Check for ID in URL query
-        todo_id = request.args.get('id')
-        todo_ref.document(todo_id).delete()
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
+    value = request.form["text"]
+    doc = notes_ref.where("text", "==", value).get()[0] 
+    notes_ref.document(doc.id).delete()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
