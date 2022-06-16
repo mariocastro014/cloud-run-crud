da6f2be6487a369e81a969a93027d3fccc9a224e
NickDeitmers <71077922+NickDeitmers@users.noreply.github.com>
2022-06-16 15:23:04 +0200

    last change

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 from flask import Flask, redirect, render_template, request, url_for
 from firebase_admin import credentials, firestore, initialize_app, _apps
+import os
 
 # Initialize Flask App
 app = Flask(__name__)
 
 # Initialize Firestore DB
 if not _apps:
-    cred = credentials.Certificate("./newkey.json") 
+    cred = credentials.Certificate(os.envriron["API_KEY"]) 
     firebase = initialize_app(cred)
 
 db = firestore.client()
